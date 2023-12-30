db40b9fb134157b9a29d31f46c4f628781b4311f
dhans <dhans@DARYANH>
2023-12-29 18:22:52 -0800

    Add project files.

---
+++ TeleporterOnly/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,16 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+
+namespace TeleporterOnly.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        private static void infiniteSprintPatch(ref float ___sprintMeter)
+        {
+            ___sprintMeter = 1f;
+        }
+    }
+}
---
+++ TeleporterOnly/TeleporterOnly.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using TeleporterOnly.Patches;
+
+namespace TeleporterOnly
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class TeleporterOnly: BaseUnityPlugin
+    {
+
+        private const string modGUID = "Daran.TeleporterOnlyMod";
+        private const string modName = "Teleporter Only Mod";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static TeleporterOnly Instance;
+
+        internal ManualLogSource logger;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            logger = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            logger.LogInfo($"The {modGUID} mod has started");
+            harmony.PatchAll(typeof(TeleporterOnly));
+            harmony.PatchAll(typeof(RoundManagerPatch));
+            // harmony.PatchAll(typeof(PlayerControllerBPatch));
+        }
+    }
+}
